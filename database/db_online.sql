3ecbdb3176ff01c16ecd8ff79cacf6cbf03c7286
Vitya Mashina <kreskiani23@gmail.com>
2018-04-15 01:50:11 +0700

    Database v.0.1

--- database/db_online.sql
+++ database/db_online.sql
@@ -1,19 +1,22 @@
 -- phpMyAdmin SQL Dump
--- version 3.2.4
--- http://www.phpmyadmin.net
+-- version 4.7.9
+-- https://www.phpmyadmin.net/
 --
--- Host: localhost
--- Generation Time: Apr 15, 2018 at 01:13 AM
--- Server version: 5.1.41
--- PHP Version: 5.3.1
+-- Host: 127.0.0.1
+-- Generation Time: Apr 14, 2018 at 08:49 PM
+-- Server version: 10.1.31-MariaDB
+-- PHP Version: 7.2.3
 
-SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Database: `db_online`
@@ -25,16 +28,21 @@ SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 -- Table structure for table `agama`
 --
 
-CREATE TABLE IF NOT EXISTS `agama` (
+CREATE TABLE `agama` (
   `AGAM_ID` int(11) NOT NULL,
-  `AGAM_NAME` varchar(15) NOT NULL,
-  PRIMARY KEY (`AGAM_ID`)
+  `AGAM_NAME` varchar(15) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `agama`
 --
 
+INSERT INTO `agama` (`AGAM_ID`, `AGAM_NAME`) VALUES
+(1, 'ISLAM'),
+(2, 'ISLAM'),
+(3, 'KRSITEN'),
+(4, 'HINDU'),
+(5, 'BUDHA');
 
 -- --------------------------------------------------------
 
@@ -42,19 +50,13 @@ CREATE TABLE IF NOT EXISTS `agama` (
 -- Table structure for table `barang_child`
 --
 
-CREATE TABLE IF NOT EXISTS `barang_child` (
-  `BACH_ID` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `barang_child` (
+  `BACH_ID` int(11) NOT NULL,
   `BACH_NAME` varchar(100) NOT NULL,
   `BACH_HARGA` int(11) NOT NULL,
   `BACH_TIMESTAMP` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `BACH_SATU_ID` int(11) NOT NULL,
-  PRIMARY KEY (`BACH_ID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
-
---
--- Dumping data for table `barang_child`
---
-
+  `BACH_SATU_ID` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -62,18 +64,12 @@ CREATE TABLE IF NOT EXISTS `barang_child` (
 -- Table structure for table `barang_parent`
 --
 
-CREATE TABLE IF NOT EXISTS `barang_parent` (
-  `BAPA_ID` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `barang_parent` (
+  `BAPA_ID` int(11) NOT NULL,
   `BAPA_NAME` varchar(100) NOT NULL,
   `BAPA_TIMESTAMP` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `BAPA_BACH_ID` int(11) NOT NULL,
-  PRIMARY KEY (`BAPA_ID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
-
---
--- Dumping data for table `barang_parent`
---
-
+  `BAPA_BACH_ID` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -81,21 +77,15 @@ CREATE TABLE IF NOT EXISTS `barang_parent` (
 -- Table structure for table `gudang_jadi`
 --
 
-CREATE TABLE IF NOT EXISTS `gudang_jadi` (
-  `GUJA_ID` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `gudang_jadi` (
+  `GUJA_ID` int(11) NOT NULL,
   `GUJA_KELUAR` int(11) NOT NULL,
   `GUJA_MASUK` int(11) NOT NULL,
   `GUJA_TOTAL` int(11) NOT NULL,
   `GUJA_TIMESTAMP` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `GUJA_BAPA_ID` int(11) NOT NULL,
-  `GUJA_BACH_ID` int(11) NOT NULL,
-  PRIMARY KEY (`GUJA_ID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
-
---
--- Dumping data for table `gudang_jadi`
---
-
+  `GUJA_BACH_ID` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -103,21 +93,15 @@ CREATE TABLE IF NOT EXISTS `gudang_jadi` (
 -- Table structure for table `gudang_tak_jadi`
 --
 
-CREATE TABLE IF NOT EXISTS `gudang_tak_jadi` (
-  `GUTA_ID` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `gudang_tak_jadi` (
+  `GUTA_ID` int(11) NOT NULL,
   `GUTA_KELUAR` int(11) NOT NULL,
   `GUTA_MASUK` int(11) NOT NULL,
   `GUTA_TOTAL` int(11) NOT NULL,
   `GUTA_TIMESTAMP` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `GUTA_BAPA_ID` int(11) NOT NULL,
-  `GUTA_BACH_ID` int(11) NOT NULL,
-  PRIMARY KEY (`GUTA_ID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
-
---
--- Dumping data for table `gudang_tak_jadi`
---
-
+  `GUTA_BACH_ID` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -125,20 +109,14 @@ CREATE TABLE IF NOT EXISTS `gudang_tak_jadi` (
 -- Table structure for table `keuangan`
 --
 
-CREATE TABLE IF NOT EXISTS `keuangan` (
-  `KEUA_ID` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `keuangan` (
+  `KEUA_ID` int(11) NOT NULL,
   `KEUA_MASUK` int(11) NOT NULL,
   `KEUA_KELUAR` int(11) NOT NULL,
   `KEUA_SALDO` int(11) NOT NULL,
   `KEUA_RINCIAN` text NOT NULL,
-  `KEUA_TIMESTAMP` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (`KEUA_ID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
-
---
--- Dumping data for table `keuangan`
---
-
+  `KEUA_TIMESTAMP` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -146,11 +124,10 @@ CREATE TABLE IF NOT EXISTS `keuangan` (
 -- Table structure for table `level`
 --
 
-CREATE TABLE IF NOT EXISTS `level` (
-  `LEVE_ID` int(11) NOT NULL AUTO_INCREMENT,
-  `LEVE_NAME` varchar(20) NOT NULL,
-  PRIMARY KEY (`LEVE_ID`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
+CREATE TABLE `level` (
+  `LEVE_ID` int(11) NOT NULL,
+  `LEVE_NAME` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `level`
@@ -159,7 +136,9 @@ CREATE TABLE IF NOT EXISTS `level` (
 INSERT INTO `level` (`LEVE_ID`, `LEVE_NAME`) VALUES
 (1, 'ADMIN'),
 (2, 'KEUANGAN'),
-(3, 'MANAGERIAL');
+(3, 'MANAGERIAL'),
+(4, 'OWNER'),
+(5, 'SUPER ADMIN');
 
 -- --------------------------------------------------------
 
@@ -167,23 +146,16 @@ INSERT INTO `level` (`LEVE_ID`, `LEVE_NAME`) VALUES
 -- Table structure for table `pegawai`
 --
 
-CREATE TABLE IF NOT EXISTS `pegawai` (
-  `PEGA_ID` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `pegawai` (
+  `PEGA_ID` int(11) NOT NULL,
   `PEGA_NAME` varchar(80) NOT NULL,
   `PEGA_EMAIL` varchar(80) NOT NULL,
   `PEGA_ALAMAT` varchar(100) NOT NULL,
   `PEGA_NO_TLP` int(13) NOT NULL,
   `PEGA_JENKEL` varchar(1) NOT NULL,
   `PEGA_TIMESTAMP` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `PEGA_AGAM_ID` int(11) NOT NULL,
-  PRIMARY KEY (`PEGA_ID`),
-  UNIQUE KEY `PEGA_EMAIL` (`PEGA_EMAIL`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
-
---
--- Dumping data for table `pegawai`
---
-
+  `PEGA_AGAM_ID` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -191,16 +163,10 @@ CREATE TABLE IF NOT EXISTS `pegawai` (
 -- Table structure for table `satuan`
 --
 
-CREATE TABLE IF NOT EXISTS `satuan` (
-  `SATU_ID` int(11) NOT NULL AUTO_INCREMENT,
-  `SATU_NAME` varchar(10) NOT NULL,
-  PRIMARY KEY (`SATU_ID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
-
---
--- Dumping data for table `satuan`
---
-
+CREATE TABLE `satuan` (
+  `SATU_ID` int(11) NOT NULL,
+  `SATU_NAME` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -208,21 +174,152 @@ CREATE TABLE IF NOT EXISTS `satuan` (
 -- Table structure for table `user`
 --
 
-CREATE TABLE IF NOT EXISTS `user` (
-  `USER_ID` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `user` (
+  `USER_ID` int(11) NOT NULL,
   `USER_NAME` varchar(50) NOT NULL,
   `USER_PASSWORD` varchar(50) NOT NULL,
   `USER_TIMESTAMP` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `USER_LEVE_ID` int(11) NOT NULL,
-  `USER_DAPE_ID` int(11) NOT NULL,
-  PRIMARY KEY (`USER_ID`),
-  UNIQUE KEY `USER_NAME` (`USER_NAME`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+  `USER_DAPE_ID` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `user`
 --
 
+INSERT INTO `user` (`USER_ID`, `USER_NAME`, `USER_PASSWORD`, `USER_TIMESTAMP`, `USER_LEVE_ID`, `USER_DAPE_ID`) VALUES
+(1, 'SUDO', '28a4437b86f15b3e4204252dd75327fe', '2018-04-14 18:48:30', 5, 0);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `agama`
+--
+ALTER TABLE `agama`
+  ADD PRIMARY KEY (`AGAM_ID`);
+
+--
+-- Indexes for table `barang_child`
+--
+ALTER TABLE `barang_child`
+  ADD PRIMARY KEY (`BACH_ID`);
+
+--
+-- Indexes for table `barang_parent`
+--
+ALTER TABLE `barang_parent`
+  ADD PRIMARY KEY (`BAPA_ID`);
+
+--
+-- Indexes for table `gudang_jadi`
+--
+ALTER TABLE `gudang_jadi`
+  ADD PRIMARY KEY (`GUJA_ID`);
+
+--
+-- Indexes for table `gudang_tak_jadi`
+--
+ALTER TABLE `gudang_tak_jadi`
+  ADD PRIMARY KEY (`GUTA_ID`);
+
+--
+-- Indexes for table `keuangan`
+--
+ALTER TABLE `keuangan`
+  ADD PRIMARY KEY (`KEUA_ID`);
+
+--
+-- Indexes for table `level`
+--
+ALTER TABLE `level`
+  ADD PRIMARY KEY (`LEVE_ID`);
+
+--
+-- Indexes for table `pegawai`
+--
+ALTER TABLE `pegawai`
+  ADD PRIMARY KEY (`PEGA_ID`),
+  ADD UNIQUE KEY `PEGA_EMAIL` (`PEGA_EMAIL`);
+
+--
+-- Indexes for table `satuan`
+--
+ALTER TABLE `satuan`
+  ADD PRIMARY KEY (`SATU_ID`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`USER_ID`),
+  ADD UNIQUE KEY `USER_NAME` (`USER_NAME`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `agama`
+--
+ALTER TABLE `agama`
+  MODIFY `AGAM_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `barang_child`
+--
+ALTER TABLE `barang_child`
+  MODIFY `BACH_ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `barang_parent`
+--
+ALTER TABLE `barang_parent`
+  MODIFY `BAPA_ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `gudang_jadi`
+--
+ALTER TABLE `gudang_jadi`
+  MODIFY `GUJA_ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `gudang_tak_jadi`
+--
+ALTER TABLE `gudang_tak_jadi`
+  MODIFY `GUTA_ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `keuangan`
+--
+ALTER TABLE `keuangan`
+  MODIFY `KEUA_ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `level`
+--
+ALTER TABLE `level`
+  MODIFY `LEVE_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `pegawai`
+--
+ALTER TABLE `pegawai`
+  MODIFY `PEGA_ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `satuan`
+--
+ALTER TABLE `satuan`
+  MODIFY `SATU_ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `USER_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
