7d3129b5c4cf47dad1271526064945662a7e2414
zzad <bagusdevil54@gmail.com>
2018-04-15 02:44:38 +0700

    2.44

---
+++ application/views/tampilan/v_content.php
@@ -0,0 +1,5 @@
+<a href="<?php echo base_url() ?> c_gudangJadi">Gudang Jadi</a>
+<a href="<?php echo base_url() ?> c_gudangSetJadi">Gudang Setengah Jadi</a>
+<a href="<?php echo base_url() ?> c_keuangan">Keuangan</a>
+<a href="<?php echo base_url() ?> C_auditStock">Audit Stock Barang</a>
+<a href="<?php echo base_url() ?> c_auditKeuangan">Audit Keuangan</a>
