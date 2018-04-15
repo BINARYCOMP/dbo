d8db53a182b6793849290753e158451af2ad8fa1
Vitya Mashina <kreskiani23@gmail.com>
2018-04-16 02:52:17 +0700

    nambah route + fix spasi di url

--- application/views/tampilan/v_content.php
+++ application/views/tampilan/v_content.php
@@ -1,5 +1,7 @@
-<a href="<?php echo base_url() ?> c_gudangJadi">Gudang Jadi</a>
-<a href="<?php echo base_url() ?> c_gudangSetJadi">Gudang Setengah Jadi</a>
-<a href="<?php echo base_url() ?> c_keuangan">Keuangan</a>
-<a href="<?php echo base_url() ?> C_auditStock">Audit Stock Barang</a>
-<a href="<?php echo base_url() ?> c_auditKeuangan">Audit Keuangan</a>
+<a href="<?php echo base_url() ?>c_gudangJadi">Gudang Jadi</a>
+<a href="<?php echo base_url() ?>c_gudangSetJadi">Gudang Setengah Jadi</a>
+<a href="<?php echo base_url() ?>c_keuangan">Keuangan</a>
+<a href="<?php echo base_url() ?>C_auditStock">Audit Stock Barang</a>
+<a href="<?php echo base_url() ?>c_auditKeuangan">Audit Keuangan</a>
+<hr>
+<a href="<?php echo base_url() ?>c_login/logout">Logout</a>
