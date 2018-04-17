1f14385cee6816c2ae1cde4ac8e286ff42a50b26
unknown <rivalmusripal@gmail.com>
2018-04-17 17:34:20 +0700

    from excel pegawai update

--- application/controllers/c_excel_pegawai.php
+++ application/controllers/c_excel_pegawai.php
@@ -20,8 +20,9 @@ class C_excel_pegawai extends CI_Controller
 
 	public function export()
 	{
+		date_default_timezone_set("Asia/Bangkok");
 		header("Content-type: application/vnd-ms-excel");
-		header("Content-Disposition: attachment; filename=Data_Pegawai.xls");
+		header("Content-Disposition: attachment; filename=Data_Pegawai_".date("l jS \of F Y h:i:s A").".xls");
 
 		$data['pegawai'] = $this->m_excel_pegawai->view();
 		$this->load->view('export_excel_pegawai', $data);
