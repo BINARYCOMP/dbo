6ae12f1c56dc2b0c5817bb4fe377aa81858054ca
zzad <bagusdevil54@gmail.com>
2018-04-18 03:43:47 +0700

    CRUD agama

--- application/models/m_viewAgama.php
+++ application/models/m_viewAgama.php
@@ -18,5 +18,23 @@ function view()
 		
 		$this->db->insert('agama',$data);
 	}
+	public function Update($data)
+	{
+		$sql="select * from agama where AGAM_ID =".$data;
+		$query=$this->db->query($sql);
+		$return = $query->result_array();
+		return $return;
+	}
+	public function UpdateData($id, $data)
+	{
+		$this->db->where('AGAM_ID', $id);
+		$this->db->update('agama', $data);
+	}
+	public function delete($id)
+	{
+		$this->db->where('AGAM_ID', $id);
+		$this->db->delete('agama');
+	}
+
 }
 ?>
