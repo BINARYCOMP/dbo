cf7ae488dd4d737bc3e0f0c002784b4bea848a55
Alfian <alfiandwi48@gmail.com>
2018-04-23 22:20:17 +0700

    style dataTables

--- application/views/tampilan/v_footer.php
+++ application/views/tampilan/v_footer.php
@@ -15,15 +15,15 @@
 <!-- Bootstrap 3.3.7 -->
 <script src="/dbo/assets/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
 <!-- DataTables -->
-<!-- <script src="/dbo/assets/bower_components/datatables.net/js/jquery.dataTables.min.js"></script>
-<script src="/dbo/assets/bower_components/datatables.net-bs/js/dataTables.bootstrap.min.js"></script> -->
+<script src="/dbo/assets/bower_components/datatables.net/js/jquery.dataTables.min.js"></script>
+<script src="/dbo/assets/bower_components/datatables.net-bs/js/dataTables.bootstrap.min.js"></script>
 <!-- SlimScroll -->
 <script src="/dbo/assets/bower_components/jquery-slimscroll/jquery.slimscroll.min.js"></script>
 <!-- FastClick -->
 <script src="/dbo/assets/bower_components/fastclick/lib/fastclick.js"></script>
 <!-- AdminLTE App -->
-<script src="/dbo/assets/dist/js/dataTables.bootstrap.js"></script>
-<script src="/dbo/assets/dist/js/jquery.dataTables.min.js"></script>
+<!-- <script src="/dbo/assets/dist/js/dataTables.bootstrap.js"></script>
+<script src="/dbo/assets/dist/js/jquery.dataTables.min.js"></script> -->
 <script type="text/javascript">
     $(function () {
         $("#lookup").dataTable();
