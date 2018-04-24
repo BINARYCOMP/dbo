f097e94237862ec94d95f655d13d1e06821dfa08
zzad <bagusdevil54@gmail.com>
2018-04-24 08:56:07 +0700

    input inventaris bach

--- application/views/v_editInventarisParent.php
+++ application/views/v_editInventarisParent.php
@@ -16,7 +16,7 @@
 			  <div class="col-md-6">
 			    <div class="box box-warning">
 			      <div class="box-header with-border">
-			        <h3 class="box-title">Edit Inventaris Parent/h3>
+			        <h3 class="box-title">Edit Inventaris Parent</h3>
 
 			        <div class="box-tools pull-right">
 			          <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i></button>
