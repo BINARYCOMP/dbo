c82e53468c61f4b9c772b77c2238110cfe0dde68
Vitya Mashina <kreskiani23@gmail.com>
2018-05-02 13:43:07 +0700

    nambahin profile di header

--- application/views/tampilan/v_header.php
+++ application/views/tampilan/v_header.php
@@ -235,7 +235,7 @@
                     <!-- Menu Footer-->
                     <li class="user-footer">
                       <div class="pull-left">
-                        <a href="#" class="btn btn-default btn-flat">Profile</a>
+                        <a href="<?php echo base_url()?>c_profil" class="btn btn-default btn-flat">Profile</a>
                       </div>
                       <div class="pull-right">
                         <a href="<?php echo base_url()?>c_login/logout" class="btn btn-default btn-flat">Sign out</a>
