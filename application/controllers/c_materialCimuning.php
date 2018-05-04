9262cd4589858f576580d6931708c8c5dba6df2c
zzad <bagusdevil54@gmail.com>
2018-05-04 23:38:11 +0700

    material cimuning

--- application/controllers/c_materialCimuning.php
+++ application/controllers/c_materialCimuning.php
@@ -2,13 +2,13 @@
 /**
  *
  */
-class C_gudangJadi extends CI_Controller
+class C_materialCimuning extends CI_Controller
 {
 
   function __construct()
   {
     parent::__construct();
-    $this->load->model('m_materialBawang');
+    $this->load->model('m_materialCimuning');
   }
   public function index()
   {
@@ -23,15 +23,15 @@ class C_gudangJadi extends CI_Controller
       $message ="";
     }
 
-      $namaParent       = $this->m_gudangJadi->getParentName();
-      $dataGudangJadi   = $this->m_gudangJadi->getDataGudang();
-      $namaKategori     = $this->m_gudangJadi->getKategoriName();
+      $namaParent       = $this->m_materialCimuning->getParentName();
+      $datamaterial     = $this->m_materialCimuning->getDataGudang();
       $data = array(
-        'namaKategori'    => $namaKategori,
-        'namaParent'      => $namaParent,
-        'dataGudangJadi'  => $dataGudangJadi,
-        'content'         => 'v_materialBawang',
+        'dataParent'      => $namaParent,
+        'datamaterial'    => $datamaterial,
         'message'         => $message,
+        'content'         => 'v_materialCimuning',
+        'title'           => 'Input Material Cimuning',
+        'menu'            => 'Input Material'
       );
       $this->load->view('tampilan/v_combine',$data);
   }
@@ -39,44 +39,42 @@ class C_gudangJadi extends CI_Controller
   {
     $parent     = $_POST['cmbParent'];
     $child      = $_POST['cmbChild'];
-    $kategori   = $_POST['cmbKategori'];
     $uraian     = $_POST['txtUraian'];
     $masuk      = $_POST['txtMasuk'];
     $keluar     = $_POST['txtKeluar'];
     $saldoAkhir = $_POST['txtSaldoAwal'] + $masuk - $keluar;
     $data = array(
-      'GUJA_KATE_ID'  => $kategori,
       'GUJA_KELUAR'   => $keluar ,
       'GUJA_URAIAN'   => $uraian ,
       'GUJA_MASUK'    => $masuk ,
       'GUJA_BAPA_ID'  => $parent ,
-      'GUJA_BACH_ID'  => $child ,
+      'GUJA_MCBA_ID'  => $child ,
       'GUJA_SALDO'    => $saldoAkhir
     );
-    $simpanBarang = $this->m_gudangJadi->simpanBarang($data, $saldoAkhir, $child);
-    echo "<script> window.location='".base_url()."c_stok?message=1' </script>";
+    $simpanMaterial = $this->m_materialCimuning->simpanMaterial($data, $saldoAkhir, $child);
+    echo "<script> window.location='".base_url()."C_materialCimuning?message=1' </script>";
   }
 
   // nama child
   public function searchChild()
   {
     $str = $_GET['q'];
-    $namaChild  = $this->m_gudangJadi->getChildName($str);
+    $namaChild  = $this->m_materialCimuning->getChildName($str);
     ?>
-      <select required name="cmbChild" id="cmbChild" onchange="showStok(this.value);" onmousemove ="showStok(this.value);" class="form-control">
+      <select required name="cmbChild" id="cmbChild" onchange="showStok();" onmousemove ="showStok();" class="form-control">
         <?php
           if ($str == 0) {
             ?>
-              <option value='0' selected>== Pilih Anak Barang ==</option>
+              <option value='0' selected>== Pilih Anak Material ==</option>
             <?php
           }else{
             ?>
-              <option value='0'>== Pilih Anak Barang ==</option>
+              <option value='0'>== Pilih Anak Material ==</option>
             <?php
           }
           foreach ($namaChild as $row){
-            echo "<option value='".$row['BACH_ID']."'>";
-            echo $row ['BACH_NAME'];
+            echo "<option value='".$row['MCCI_ID']."'>";
+            echo $row ['MCCI_NAME'];
            echo "</option>";
           }
         ?>
@@ -87,62 +85,64 @@ class C_gudangJadi extends CI_Controller
   // cari stok
   public function searchStok()
   {
-    $str = $_GET['q'];
-    $stokAwal = $this->m_gudangJadi->getFirstStock($str);
-    if ($str == 0) {
+    $mpciId = $_GET['mpciId'];
+    $mcciId = $_GET['mcciId'];
+    $stokAwal = $this->m_materialCimuning->getFirstStock($mcciId,$mpciId);
+    if ($mpciId == 0 || $mcciId == 0 ) {
       ?>
-        <input type="text"  class="form-control" name="txtSaldoAwal" id="saldoAwal" required readonly placeholder="0"> 
+        <input type="text"  class="form-control" name="txtSaldoAwal" id="saldoAwal" required readonly placeholder="0" value="0"> 
       <?php
     }else{
-      ?>
-        <input type="text" class="form-control"  name="txtSaldoAwal" id="saldoAwal" required readonly value="<?php echo $stokAwal[0]['BACH_GUJA_TOTAL'] ?>"> 
-      <?php
+      if (isset($stokAwal[0]['MACI_SALDO'])) {
+        ?>
+          <input type="text" class="form-control"  name="txtSaldoAwal" id="saldoAwal" required readonly value="<?php echo $stokAwal[0]['MACI_SALDO'] ?>"> 
+        <?php
+      }else{
+        ?>
+          <input type="text" class="form-control"  name="txtSaldoAwal" id="saldoAwal" required readonly value="<?php echo '0' ?>"> 
+        <?php
+      }
     }
   }
   public function modalKonfirmasi()
   {
     $cmbParent     = $_GET['parent'];
     $cmbChild      = $_GET['child'];
-    $cmbKategori   = $_GET['kategori'];
     $txtUraian     = $_GET['keterangan'];
     $txtMasuk      = $_GET['masuk'];
     $txtKeluar     = $_GET['keluar'];
     $txtSaldoAwal  = $_GET['awal'];
-    $saldoAkhir    = $txtSaldoAwal + $txtMasuk - $txtKeluar;
-
+    $saldoAkhir    = $txtSaldoAwal + intval($txtMasuk) - intval($txtKeluar);
     ?>
       <div class="modal-content">
         <div class="modal-header">
           <button type="button" class="close" data-dismiss="modal" aria-label="Close">
             <span aria-hidden="true">&times;</span></button>
-          <h4 class="modal-title">Input Barang Setengah Jadi</h4>
+          <h4 class="modal-title">Input Material Setengah Jadi</h4>
         </div>
         <div class="modal-body">
           <table class="table">
             <tr>
-              <th>Induk Barang</th>
-              <th>Anak Barang</th>
-              <th>Kategori</th>
-              <th>Barang Masuk</th>
-              <th>Barang Keluar</th>
+              <th>Induk Material</th>
+              <th>Anak Material
+              <th>Material Masuk</th>
+              <th>Material Keluar</th>
               <th>Saldo Akhir</th>
             </tr>
             <tr>
               <td><?php echo $cmbParent ?></td>
               <td><?php echo $cmbChild ?></td>
-              <td><?php echo $cmbKategori ?></td>
-              <td><?php echo $txtMasuk ?></td>
-              <td><?php echo $txtKeluar ?></td>
+              <td><?php echo intval($txtMasuk) ?></td>
+              <td><?php echo intval($txtKeluar) ?></td>
               <td><?php echo $saldoAkhir ?> </td>
             </tr>
           </table>
         </div>
         <div class="modal-footer">
           <button type="button" class="btn btn-outline pull-left" data-dismiss="modal">Close</button>
-          <form action="<?php echo base_url()?>c_gudangJadi/inputStok" method="POST">
+          <form action="<?php echo base_url()?>c_material/inputStok" method="POST">
             <input type="hidden" name="cmbParent" value="<?php echo $cmbParent?>">
             <input type="hidden" name="cmbChild" value="<?php echo $cmbChild?>">
-            <input type="hidden" name="cmbKategori" value="<?php echo $cmbKategori?>">
             <input type="hidden" name="txtMasuk" value="<?php echo $txtMasuk?>">
             <input type="hidden" name="txtKeluar" value="<?php echo $txtKeluar?>">
             <input type="hidden" name="txtUraian" value="<?php echo $txtUraian?>">
@@ -156,13 +156,21 @@ class C_gudangJadi extends CI_Controller
   }
 
   public function modalChild()
-   {
-     $cmbParent = $_GET['parent'];
-     $namaChild = $this->m_gudangJadi->getChildByBapaId($cmbParent);
-     $data = array(
-      'cmbParent' => $cmbParent ,
-      'namaChild' => $namaChild 
-    );
-     $this->load->view('modal/v_modalChildGudangJadi', $data);
+  {
+    $cmbParent = $_GET['parent'];
+    $namaChild = $this->m_materialCimuning->getChildBympciId($cmbParent);
+    if (is_array($namaChild) || is_object($namaChild)){
+      $no=1;
+      foreach ($namaChild as $row) {
+        ?>
+          <tr style="cursor: pointer;" class="isi2" data-brgChild="<?php echo $row['MCCI_ID']; ?>">
+            <td> <?php echo $no ?> </td>
+            <td> <?php echo $row['MCCI_NAME']?> </td>
+            <td> <?php echo $row['SATU_NAME']?> </td>
+          </tr>
+        <?php
+        $no++;
+      }
+    }
   } 
 }
